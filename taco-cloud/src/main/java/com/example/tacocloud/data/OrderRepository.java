54348aa8795a32209bacfc098100945720c3fa27
hobossa <hoboss@gmail.com>
2022-05-17 15:06:21 -0700

    add data.sql for JPA, and fixed some bug.

---
+++ taco-cloud/src/main/java/com/example/tacocloud/data/OrderRepository.java
@@ -0,0 +1,7 @@
+package com.example.tacocloud.data;
+
+import com.example.tacocloud.Order;
+import org.springframework.data.repository.CrudRepository;
+
+public interface OrderRepository extends CrudRepository<Order, Long> {
+}
